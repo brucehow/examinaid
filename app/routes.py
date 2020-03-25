59d099a1a8438b75cf641ef794fe55128122da2e
ForsakenIdol <lachlandwhang@gmail.com>
2020-03-25 22:28:52 +0800

    Add Flask application folder and file outlines

---
+++ app/routes.py
@@ -0,0 +1,9 @@
+from app import app
+from flask import render_template
+
+@app.route("/")
+@app.route("/index")
+def index():
+    site_title = "General Kenobi"
+    welcome_string = "Hello World! Welcome to this skeleton of an application."
+    return render_template("index.html", welcome_string=welcome_string, site_title=site_title)
