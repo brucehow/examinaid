2d5252d253dfdf11237626b08415a43e87154434
ForsakenIdol <lachlandwhang@gmail.com>
2020-03-29 15:48:11 +0800

    Add basic form validation, flash message in place of login submit

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.forms import LoginForm
 
 @app.route("/")
@@ -18,7 +18,10 @@ def about():
     name = "Nobody"
     return render_template("about.html", name=name)
 
-@app.route("/login")
+@app.route("/login", methods = ["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
