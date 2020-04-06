fd63bbfd86ff4cfebd8846e2b4308cda47bca7f9
ForsakenIdol <lachlandwhang@gmail.com>
2020-04-06 13:38:39 +0800

    Add url_for() function links to replace relative links

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm, RegisterForm
 
 @app.route("/")
