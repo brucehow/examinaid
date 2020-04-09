4a756ae663c345a28dc1a7488118a1f606862ca8
ForsakenIdol <lachlandwhang@gmail.com>
2020-04-09 10:21:17 +0800

    Change element ids to be more descriptive and informative

--- app/static/scripts/contact-form.js
+++ app/static/scripts/contact-form.js
@@ -2,13 +2,18 @@ $(document).ready(() => {
   console.log("Form ready!");
 
   const scriptURL = 'https://script.google.com/macros/s/AKfycbzBHOF2hcp7c2cWBWc8r2QoeCMHU6HGvN3pCFBlFG0UiPzwdP4/exec'
-  const form = document.forms['submit-to-google-sheet']
+  const form = document.forms['contact-to-google-sheets']
 
   form.addEventListener('submit', target => {
     target.preventDefault();
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message));
+      .then(response => {
+        console.log('Success!', response);
+        document.getElementById("google-sheets-form").reset();
+      })
+      .catch(error => {
+        console.error('Error!', error.message);
+      });
   });
 
 });
