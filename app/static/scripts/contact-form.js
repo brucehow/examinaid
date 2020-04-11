255ab8de64d9d95ace8a98aaa17c85a9e2e78ac4
ForsakenIdol <lachlandwhang@gmail.com>
2020-04-11 23:17:02 +0800

    Add temporary contact message acknowledgement

--- app/static/scripts/contact-form.js
+++ app/static/scripts/contact-form.js
@@ -1,6 +1,8 @@
 $(document).ready(() => {
   console.log("Form ready!");
 
+  $("#contact-form-response").hide();
+
   const scriptURL = 'https://script.google.com/macros/s/AKfycbzBHOF2hcp7c2cWBWc8r2QoeCMHU6HGvN3pCFBlFG0UiPzwdP4/exec'
   const form = document.forms['contact-to-google-sheets']
 
@@ -10,6 +12,10 @@ $(document).ready(() => {
       .then(response => {
         console.log('Success!', response);
         document.getElementById("google-sheets-form").reset();
+        $("#contact-form-response").fadeToggle(500);
+        setTimeout(() => {
+          $("#contact-form-response").fadeToggle(500);
+        }, 5000)
       })
       .catch(error => {
         console.error('Error!', error.message);
