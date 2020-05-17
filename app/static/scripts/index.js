b59bf576bd08e4efe54a5d8e09f2d11cce24ec27
ForsakenIdol <lachlandwhang@gmail.com>
2020-05-17 14:58:51 +0800

    Custom file upload button for tests

--- app/static/scripts/index.js
+++ app/static/scripts/index.js
@@ -6,5 +6,14 @@ $(document).ready(() => {
     target.preventDefault();
     console.log($("#example-test-form").serialize());
   });
-
+  
+  $("#test-file-upload").on("change", () => {
+    if ($("#test-file-upload").get(0).files.length === 0) console.log("No files!");
+    else {
+      var filename = document.getElementById("test-file-upload").value.split("\\");
+      console.log(filename);
+      document.getElementById("name-of-file").innerHTML = filename[filename.length - 1];
+    }
+  });
+  
 });
