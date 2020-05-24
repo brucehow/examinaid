cedba2013ff4fa6676833d962f51cb9fb360691e
Paul <paulosullivanuwa@gmail.com>
2020-05-24 15:32:28 +0800

    update output string of feedback

--- app/routes.py
+++ app/routes.py
@@ -501,12 +501,13 @@ def submit():
 
       output = "achieved {} of {} for all automatic questions. The remaining questions will be manually marked.\n".format(marksAchieved,sum(availMarksauto))
 
-      print(output)
       for i in range (0,len(incorrectQnumber)):
-        print("You incorrectly answered:Q{}: {}\n You answered:{}\n Correct Answer: {}\n".format(incorrectQnumber[i],incorrectQuestion[i],youAnswered[i],correctAnswers[i]))
+        output += "You incorrectly answered: Q{}: {}\n You answered: {}\n Correct Answer: {}\n".format(incorrectQnumber[i],incorrectQuestion[i],youAnswered[i],correctAnswers[i])
 
 
     return redirect(url_for('userprofile'))
+
+
 
 # Admin manage student logins
 @app.route('/manageusers')
